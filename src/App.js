54299cb2adc1f1073e3b4322de46fa46471eb8c2
Айдар <bajanga07@gmail.com>
2021-03-13 13:51:38 +0700

    final version
    added filtration, comments, little loading screen before table display

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import ReactPaginate from 'react-paginate';
 import './App.css';
 
 function Table(props) {
+	//data отвечает за хранение текущих 50-ти данных
 	const [data, setData] = useState([...props.dataCurrent]);
+	//filter хранит строку по которой пройдет фильтрация
+	const [filter, setFilter] = useState('');
+	//sortConfig объект в который задает направление сортировки и ключ по которому будет идти сорртировка
 	const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
-	
+
+	//обновляем переменные при новом рендере
+	useEffect(() => {
+		setData(props.dataCurrent);
+		setSortConfig({ key: null, direction: null });
+		setFilter(props.filter);
+	}, [props.dataCurrent, props.filter]);
+
+	//установка класса в зависимости от текущего sortConfig
 	function classSetter(name) {
 		if (!sortConfig) {
 			return;
@@ -13,6 +25,7 @@ function Table(props) {
 		return sortConfig.key === name ? sortConfig.direction : undefined;
 	}
 
+	//задание нового sortConfig для сортировки
 	function requestSort(key) {
 		let direction = 'ascending';
 		if (sortConfig.key === key && sortConfig.direction === 'ascending') {
@@ -21,17 +34,16 @@ function Table(props) {
 		setSortConfig({ key, direction });
 	}
 
-	useEffect(() => {
-		setData(props.dataCurrent);
-		setSortConfig({ key: null, direction: null });
-	}, [props.dataCurrent]);
-
+	//при обновлении sortConfig вызываем функцию сортировки
 	useEffect(() => {
 		sortBy();
 	}, [sortConfig]);
 
+	//функция сортировки
 	function sortBy() {
+		//создаем копию массива
 		let sortedProducts = [...data];
+		//сортировка в зависимости от направлния
 		sortedProducts.sort((a, b) => {
 			if (a[sortConfig.key] < b[sortConfig.key]) return sortConfig.direction === 'ascending' ? -1 : 1;
 			if (a[sortConfig.key] > b[sortConfig.key]) return sortConfig.direction === 'ascending' ? 1 : -1;
@@ -60,28 +72,37 @@ function Table(props) {
 			</thead>
 			<tbody>
 				{
-					data.length
-						? data.map((d) => (
-							d.show ?
-								<tr key={d.id}>
-									<td>{d.index + 1}</td>
-									<td>{d.fName}</td>
-									<td>{d.sName}</td>
-								</tr>
-								: null
+					//сначала идет фильтрация массива, затем сортировка по нужному столбцу
+					data
+						.filter((d) =>
+							d.fName.toUpperCase().indexOf(filter.toUpperCase()) !== -1 || d.sName.toUpperCase().indexOf(filter.toUpperCase()) !== -1
+						)
+						.map((d) => (
+
+							<tr key={d.id} >
+								<td>{d.index + 1}</td>
+								<td>{d.fName}</td>
+								<td>{d.sName}</td>
+							</tr>
 						))
-						: <tr><td><h6>Loading...</h6></td></tr>
 				}
 			</tbody>
 		</table>);
 }
 
 function App() {
+	//data хранит массив из 1000 объектов
 	const [data, setData] = useState([]);
+	//pageСount количество страниц для пагинации
 	const [pageCount, setPageCount] = useState(0);
+	//filterText хранит строку которую вводит пользователь для фильтрации
+	const [filterText, setFilterText] = useState('');
+	//количество элементов за страницу
 	const perPage = 50;
-	const [pageIndexes, setPageIndexes] = useState({ start: 0, end: 50 });
+	//pageIndexes опрделяют начало и конец отрезка массива data, для передачи в компонент Table
+	const [pageIndexes, setPageIndexes] = useState({ start: 0, end: perPage });
 
+	// при прогрузке загрузить жсон url
 	useEffect(() => {
 		async function getData() {
 			let res = await fetch('https://next.json-generator.com/api/json/get/Ek5OUQ3M5');
@@ -92,20 +113,22 @@ function App() {
 		getData();
 	}, []);
 
+	//при изменении страницы обновить начало и конец отрезка
 	function handlePageChange(e) {
+		//e.selected возвращает выбранную страницу
 		let elementStart = e.selected * perPage;
 		setPageIndexes({ start: elementStart, end: elementStart + perPage });
 	}
 
 	function filterTable(e) {
-		console.log(e.target.value);
+		setFilterText(e.target.value);
 	}
 	return (
-		<div>
+		<div className="App">
 			{
 				data.length
-					? (<><input type="text" onChange={filterTable} />
-						<Table dataCurrent={data.slice(pageIndexes.start, pageIndexes.end)}></Table>
+					? (<><input type="text" onChange={filterTable} placeholder="Filter..." />
+						<Table dataCurrent={data.slice(pageIndexes.start, pageIndexes.end)} filter={filterText}></Table>
 						<ReactPaginate
 							previousLabel={"<<"}
 							nextLabel={">>"}
@@ -122,11 +145,15 @@ function App() {
 							breakLinkClassName={'pagination__page'}
 						/>
 					</>)
-					: <p>Loading...</p>
+					: (<div className="loading">
+						<div className="loading__wrapper">
+							<p className="loading__name">Loading...</p>
+							<div className="lds-dual-ring"></div>
+						</div>
+					</div>)
 			}
 		</div>
 	);
-
 }
 
 export default App;
